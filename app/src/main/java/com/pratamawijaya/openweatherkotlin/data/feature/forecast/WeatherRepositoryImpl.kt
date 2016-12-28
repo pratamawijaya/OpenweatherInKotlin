016de850ac3c5751e87296bc78615f3ede163deb
pratamawijaya <set.mnemonix@gmail.com>
2016-12-28 10:23:28 +0700

    remove gradle.properties

--- app/src/main/java/com/pratamawijaya/openweatherkotlin/data/feature/forecast/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/pratamawijaya/openweatherkotlin/data/feature/forecast/WeatherRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.pratamawijaya.openweatherkotlin.data.feature.forecast
 
+import android.util.Log
 import com.pratamawijaya.openweatherkotlin.data.Request
 import com.pratamawijaya.openweatherkotlin.domain.repository.WeatherRepository
 
@@ -12,6 +13,7 @@ class WeatherRepositoryImpl : WeatherRepository {
 
   override fun getForecast(cityName: String, apiKey: String): String {
     var url = "http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&mode=json&appid=${apiKey}"
+    Log.d("url", "request : " + url)
     return Request().run(url)
   }
 
