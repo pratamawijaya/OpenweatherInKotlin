03ab35729eb09fdbb4f2cab7bda3c6ffbd8a6699
pratamawijaya <set.mnemonix@gmail.com>
2016-12-30 16:38:59 +0700

    revise UI item load icon

--- app/src/main/java/com/pratamawijaya/openweatherkotlin/presentation/ui/adapter/MainAdapter.kt
+++ app/src/main/java/com/pratamawijaya/openweatherkotlin/presentation/ui/adapter/MainAdapter.kt
@@ -1,14 +1,18 @@
 package com.pratamawijaya.openweatherkotlin.presentation.ui.adapter
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pratamawijaya.openweatherkotlin.R
 import com.pratamawijaya.openweatherkotlin.data.Forecast
 import com.pratamawijaya.openweatherkotlin.presentation.ui.adapter.MainAdapter.MainHolder
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_forecast.view.imgItem
 import kotlinx.android.synthetic.main.item_forecast.view.tvItemDate
-import kotlinx.android.synthetic.main.item_forecast.view.tvItemTemperature
+import kotlinx.android.synthetic.main.item_forecast.view.tvItemTemperatureMax
+import kotlinx.android.synthetic.main.item_forecast.view.tvItemTemperatureMin
 import kotlinx.android.synthetic.main.item_forecast.view.tvItemWeather
 
 /**
@@ -35,7 +39,15 @@ class MainAdapter(val forecasts: List<Forecast>) : RecyclerView.Adapter<MainHold
       with(forecasts) {
         itemView.tvItemDate.text = dt_txt
         itemView.tvItemWeather.text = weather[0].description
-        itemView.tvItemTemperature.text = main.temp.toString()
+        itemView.tvItemTemperatureMax.text = main.temp_max.toString()
+        itemView.tvItemTemperatureMin.text = main.temp_min.toString()
+
+        val iconUrl = "http://openweathermap.org/img/w/${forecasts.weather[0].icon}.png"
+        Log.d("debug", "icon " + iconUrl)
+
+        Picasso.with(itemView.context)
+            .load(iconUrl)
+            .into(itemView.imgItem)
       }
     }
 
